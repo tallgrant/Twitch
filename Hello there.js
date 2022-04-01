7bbed35b31e661a88732e46e4350d497a4e5076c
tallgrant <102809569+tallgrant@users.noreply.github.com>
2022-04-01 09:39:22 -0500

    Update Hello there.js

--- Hello there.js
+++ Hello there.js
@@ -1,3 +1,2 @@
 a == "Torqueish";
-if (a == "Torqueish") {"Hello Fearless Leader"}; 
-else {"You are hearing me talk"}
+if (a == "Torqueish") {"Hello Fearless Leader"} else {"You are hearing me talk"}
